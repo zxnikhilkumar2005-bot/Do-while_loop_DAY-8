2a60905bff3c9741376d88617e01e6b136a0cc88
Nikhil kumar <zxnihilkumar2005@gmail.com>
2026-02-24 03:36:00 +0530

    ___

--- app.js
+++ app.js
@@ -19,3 +19,84 @@ const prompt = require("prompt-sync")();
 
 
 // Q 33. Sasta Calculator 
+// let n;
+// do {
+//     console.log("Enter 1 for addition ");
+//     console.log("Enter 1 for subtraction");
+//     n =Number(prompt("Enter a number"))
+//     switch
+// }while()
+
+// let n = Number(prompt("Enter a number: "));
+// let temp = n;//18 
+// let sum = 0;
+
+// do {
+//     let digit = temp % 10;   // last digit 18 % 10 = 8 
+//     sum += digit;            // add in sum
+//     temp = Math.floor(temp / 10); // remove last digit
+// } while (temp > 0);
+
+// // Check Harshad
+// if (n % sum === 0) {
+//     console.log("Harshad Number");
+// } else {
+//     console.log("Not a Harshad Number");
+// }
+
+//  Abundant Number 
+
+// let n = Number(prompt("Enter a number: "));
+// let i = 1;
+// let sum = 0;
+
+// do {
+//     if (n % i === 0) {
+//         sum += i;  
+//     }
+//     i++;
+// } while (i < n);   
+
+// if (sum > n) {
+//     console.log("Abundant Number");
+// } else {
+//     console.log("Not an Abundant Number");
+// }
+
+// let n = Number(prompt("Enter a number: "));
+
+// if (n <= 1) {
+//     console.log("No prime factors");
+// } else {
+//     let i = 2;
+
+//     do {
+//         if (n % i === 0) {
+//             console.log(i);   
+//             n = n / i;       
+//         } else {
+//             i++;             
+//         }
+//     } while (n > 1);
+// }
+
+class Solution {
+    isNeon(n) {
+        let square = n * n;
+        let sum = 0;
+
+        // square ke digits ka sum nikalna
+        do {
+            sum += square % 10;
+            square = Math.floor(square / 10);
+        } while (square > 0);
+
+        if (sum === n) {
+            return "Yes";
+        } else {
+            return "No";
+        }
+    }
+}
+
+module.exports = Solution;
